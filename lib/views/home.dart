198c466b5f8770cd740bbbd7b1db944f6325c813
parables <parables95@gmail.com>
2021-10-17 18:21:02 +0000

    Added category list and appbar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,18 +8,63 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFFFF0DA),
       body: SafeArea(
-        child: Container(
-          height: 240,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, int itemIndex) {
-              return FoodCardWidget();
-            },
-            separatorBuilder: (BuildContext context, separatorIndex) {
-              return SizedBox(width: 20);
-            },
-            itemCount: 10,
-          ),
+        child: ListView(
+          padding: EdgeInsets.all(15),
+          children: [
+            ListTile(
+              title: Text(
+                'Home',
+                style: TextStyle(fontSize: 25),
+              ),
+              subtitle: Text('Explore food'),
+              trailing: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network('https://i.pravatar.cc/150?img=11'),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 120,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int itemIndex) {
+                  return CategoryCardWidget();
+                },
+                separatorBuilder: (BuildContext context, separatorIndex) {
+                  return SizedBox(width: 20);
+                },
+                itemCount: 10,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Noodles',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text('See all'),
+              ],
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 240,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int itemIndex) {
+                  return FoodCardWidget();
+                },
+                separatorBuilder: (BuildContext context, separatorIndex) {
+                  return SizedBox(width: 20);
+                },
+                itemCount: 10,
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -100,6 +145,53 @@ class FoodCardWidget extends StatelessWidget {
             width: 150,
             height: 150,
             padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Color(0xFFFEF7EA),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://res.cloudinary.com/icodegh/image/upload/v1634329745/food_app/b_pork.jpg',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+          bottomLeft: Radius.circular(100),
+          bottomRight: Radius.circular(100),
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              'Noodles',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Container(
+            width: 80,
+            height: 80,
+            padding: EdgeInsets.all(8),
             decoration: BoxDecoration(
               color: Color(0xFFFEF7EA),
               borderRadius: BorderRadius.circular(100),
